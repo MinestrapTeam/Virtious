f41decaf81393d155423faceaa5ea328d0922967
Domenic Portuesi <sobiohazardous@gmail.com>
2013-08-01 20:30:15 -0400

    Added block and items ref

---
+++ teamm/mods/virtious/lib/VirtiousBlocks.java
@@ -0,0 +1,12 @@
+package teamm.mods.virtious.lib;
+
+public class VirtiousBlocks 
+{
+	/**
+	 * Loads all block objects
+	 */
+	public void loadAllBlocks()
+	{
+		//TODO add all block objects here
+	}
+}
---
+++ teamm/mods/virtious/lib/VirtiousItems.java
@@ -0,0 +1,12 @@
+package teamm.mods.virtious.lib;
+
+public class VirtiousItems 
+{
+	/**
+	 * Loads all item objects
+	 */
+	public void loadAllItems()
+	{
+		//TODO load all item objects
+	}
+}
