b34830fc2d42a3f54903d7ae0e87d68e4b8daa6e
Domenic Portuesi <sobiohazardous@gmail.com>
2013-08-02 18:02:54 -0400

    Added Tool Logic

---
+++ teamm/mods/virtious/item/VirtiousAxe.java
@@ -0,0 +1,25 @@
+package teamm.mods.virtious.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
+
+public class VirtiousAxe extends ItemVirtiousTool
+{
+    /** an array of the blocks this axe is effective against */
+    public static final Block[] blocksEffectiveAgainst = new Block[] {Block.planks, Block.bookShelf, Block.wood, Block.chest, Block.stoneDoubleSlab, Block.stoneSingleSlab, Block.pumpkin, Block.pumpkinLantern};
+
+    public VirtiousAxe(int par1, EnumVirtiousToolMaterial par2EnumVirtiousToolMaterial)
+    {
+        super(par1, 3.0F, par2EnumVirtiousToolMaterial, blocksEffectiveAgainst);
+    }
+
+    /**
+     * Returns the strength of the stack against a given block. 1.0F base, (Quality+1)*2 if correct blocktype, 1.5F if
+     * sword
+     */
+    public float getStrVsBlock(ItemStack par1ItemStack, Block par2Block)
+    {
+        return par2Block != null && (par2Block.blockMaterial == Material.wood || par2Block.blockMaterial == Material.plants || par2Block.blockMaterial == Material.vine) ? this.efficiencyOnProperMaterial : super.getStrVsBlock(par1ItemStack, par2Block);
+    }
+}
