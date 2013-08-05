f2917f8b8cf70c9c088b7fcffdc245627a541c8d
Dawars <dawars00@gmail.com>
2013-08-05 02:28:29 +0200

    Ocean mob spawn disabled

--- teamm/mods/virtious/world/biome/BiomeGenVirtiousOcean.java
+++ teamm/mods/virtious/world/biome/BiomeGenVirtiousOcean.java
@@ -1,6 +1,8 @@
 package teamm.mods.virtious.world.biome;
 
+import teamm.mods.virtious.entity.EntityBurfalaunt;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.biome.SpawnListEntry;
 
 public class BiomeGenVirtiousOcean extends BiomeGenBase
 {
@@ -10,5 +12,10 @@ public class BiomeGenVirtiousOcean extends BiomeGenBase
         this.spawnableCreatureList.clear();
         this.setBiomeName("Virtious Acid Ocean");
         this.setMinMaxHeight(-1.0F, 0.4F);
+        
+        this.spawnableMonsterList.clear();
+        this.spawnableCreatureList.clear();
+        this.spawnableWaterCreatureList.clear();
+        this.spawnableCaveCreatureList.clear();
     }
 }
